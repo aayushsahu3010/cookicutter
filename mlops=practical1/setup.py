eb52ac03a9959a1cb18d6cfa67598c9dd6ac2ba0
aayush <ayushsahu3010@gmail.com>
2025-09-29 15:00:34 +0530

    commit files using cookiecutter

---
+++ mlops=practical1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a repo for practical use of mlops',
+    author='aayushsahu3010',
+    license='',
+)
